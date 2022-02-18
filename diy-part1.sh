784ec9c42a3f2be99c3471386e135d7dfa4c38f2
molezz <molezz@gmail.com>
2022-02-18 08:57:09 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,7 +17,12 @@
 # sed -i '$a src-git fw876 https://github.com/fw876/helloworld' feeds.conf.default
 
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-sed -i '$a src-git fw876 https://github.com/fw876/helloworld' feeds.conf.default
-sed -i '$a src-git kenzok8 https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+# sed -i '$a src-git fw876 https://github.com/fw876/helloworld' feeds.conf.default
 
+
+#sed -i '$a src-git kenzok8 https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+#sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+sed -i '$a src-git packages https://git.openwrt.org/feed/packages' feeds.conf.default
+sed -i '$a src-git luci https://git.openwrt.org/project/luci' feeds.conf.default
+sed -i '$a src-git routing https://git.openwrt.org/feed/routing' feeds.conf.default
+sed -i '$a src-git kiddin9 https://github.com/kiddin9/openwrt-packages' feeds.conf.default
